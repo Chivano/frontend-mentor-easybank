1e7d599ac6a1cc7df15eecce59618b15028f378a
Chivano <zizzozz@hotmail.com>
2021-06-15 13:04:32 +0200

    First component with svelte

---
+++ src/components/Navbar/Menu.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<ul class="menu">
+  <li>Home</li>
+  <li>About</li>
+  <li>Contact</li>
+  <li>Blog</li>
+  <li>Careers</li>
+</ul>
+
+<style>
+  .menu {
+    list-style: none;
+  }
+</style>
---
+++ src/components/RequestButton/Button.svelte
@@ -0,0 +1,23 @@
+<script>
+  function handleClick() {
+    console.log("Request invite");
+  }
+</script>
+
+<button class="button" on:click={handleClick}>Request invite</button>
+
+<style>
+  .button {
+    color: white;
+    font-family: Public Sans;
+    font-weight: 400;
+    font-size: 18px;
+    border-radius: 2rem;
+    padding: 0.5rem 1.5rem 0.5rem 1.5rem;
+    background: linear-gradient(
+      90deg,
+      rgba(49, 211, 92, 1) 0%,
+      rgba(43, 183, 218, 1) 89%
+    );
+  }
+</style>
